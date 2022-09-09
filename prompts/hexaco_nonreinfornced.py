dc70fc55f87e956c56665efa41ef6bb4b0ee88c8
ncrossberg <90328132+ncrossberg@users.noreply.github.com>
2022-09-09 12:06:18 +0200

    Update hexaco_nonreinfornced.py

--- prompts/hexaco_nonreinfornced.py
+++ prompts/hexaco_nonreinfornced.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 11:58:07 2022
+
+@author: nicolarossberg
+"""
 import pandas as pd
 import openai
 
-openai.api_key = "KEY"
+openai.api_key = "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"
 
 
 #meta params.
@@ -40,7 +47,6 @@ QL = ["I would be quite bored by a visit to an art gallery.", "I plan ahead and 
       "When I’m in a group of people, I’m often the one who speaks on behalf of the group.", "I remain unemotional even in situations where most people get very sentimental.",
       "I’d be tempted to use counterfeit money, if I were sure I could get away with it."]
 
-
 # Create list of desired indexes
 index_list = ['sex', 'age']
 for n in range(1, 61):
@@ -50,92 +56,80 @@ for n in range(1, 61):
 #temp_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 temp_list = [0.2]
 
+col_list = []
+for n in range(len(QL)):
+    col_list.append('Q' + str(n+1))
 
-# Iterating through every temperature
 for temp in temp_list:
-    # Create dataframe for every temperature:
-    tempcurrent = pd.DataFrame()
-    prompt_tempcurrent = pd.DataFrame()
-    # Chaning indices for clarity
-    # tempcurrent.index = index_list
-    tempcurrent.index = range(n_runs)
-    prompt_tempcurrent.index = range(n_runs)
-    #Create list for sex
-    sex_list = []
-    prompt_sex_list = []
-    #Request n_runs ages
-    response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt='What is your gender?',
-        temperature=temp,
-        max_tokens=max_tokens_meta,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        n = n_runs)
-    #Add sexes to list
-    for n in range(n_runs):
-        sex = str(response['choices'][n].text)
-        sex_list.append(sex[2:])
-        prompt_sex_list.append('What is your gender?')
-    #Add sexes to Dataframe:
-    tempcurrent['sex'] = sex_list
-    prompt_tempcurrent['sex'] = prompt_sex_list
-    #Create age list
-    age_list = []
-    prompt_age_list = []
-    #Request n_run ages
-    response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt='How old are you?',
-        temperature=temp,
-        max_tokens=max_tokens_meta,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        n=n_runs)
-    #Convert age-statements into int and append to list
-    for n in range(n_runs):
-        age = str(response['choices'][n].text)
-        age_list.append([int(s) for s in age.split() if s.isdigit()][0])
-        prompt_age_list.append('How old are you?')
-    #Add ages to Dataframe:
-    tempcurrent['age'] = age_list
-    prompt_tempcurrent['age'] = prompt_age_list
-    #Set i for enumerating purposes:
-    i = 1
-    for question in QL:
-        # General query
-        query = "Below is a statement about you. Please read it and decide how much you agree or disagree with that statement. Write your response using the following scale:/n/n5 = strongly agree/n4 = agree/n3 = neutral/n2 = disagree/n1 = strongly disagree./n/nPlease answer the statement, even if you are not completely sure of your response./n/nStatement:"
-        response_prompt = "\nResponse:"
-        # Complete query
-        fullquestion = query + question + response_prompt
-        #Created List for current question
-        question_list = []
-        prompt_question_list = []
-        # Ask AI for output
-        response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=fullquestion,
-            temperature=temp,
-            max_tokens=max_tokens_meta,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            n = n_runs)
-        #Convert responses to list of responses
-        for n in range(n_runs):
-            prompt_question_list.append(question)
-            resp = str(response['choices'][n].text)
-            #Isolate number
-            question_list.append(int(resp[-1]))
-        #Append answers to question to dataframe
-        tempcurrent['Q' +str(i)] = question_list
-        prompt_tempcurrent['Q' + str(i)] = prompt_question_list
-        #Increase i by one (indicating next question for enumeration)
-        i += 1
-    # Save temperature dataframe as csv
-    filename = "hexaco_non_reinforced_temperature_" + str(temp) + '.csv'
-    prompt_name = "hexaco_non_reinfornced_prompt_temperature" + str(temp) + '.csv'
-    tempcurrent.to_csv(filename)
-    prompt_tempcurrent.to_csv(prompt_name)
+    if temp == 0.0:
+        # Answers for temperature = 0
+        # Generate 1 row df
+        temp0 = pd.DataFrame()
+        temp0.index = range(1)
+        # Iterate through questions
+        for question in QL:
+            # General query
+            query = "Below is a statement about you. Please read it and decide how much you agree or disagree with that statement. Write your response using the following scale:/n/n5 = strongly agree/n4 = agree/n3 = neutral/n2 = disagree/n1 = strongly disagree./n/nPlease answer the statement, even if you are not completely sure of your response./n/nStatement:"
+            response_prompt = "\nResponse:"
+            # Complete query
+            fullquestion = query + question + response_prompt
+            # Ask AI for output
+            response = openai.Completion.create(
+                model="text-davinci-002",
+                prompt=fullquestion,
+                temperature=0.0,
+                max_tokens=max_tokens_meta,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                n=1)
+
+            # Store answers into df
+            answer = str(response['choices'][0].text)
+            # Append current run answers to current temperature dataframe
+            temp0[question] = answer
+        # Cahnging columns' name for clarity
+        temp0.columns = col_list
+        # Save temperature dataframe as csv
+        filename = "hexaco_temperature_0.0.csv"
+        temp0.to_csv(filename, index=False)
+
+    else:
+        # Create dataframe for every temperature:
+        tempcurrent = pd.DataFrame()
+        # Changing indices for clarity
+        tempcurrent.index = range(n_runs)
+        # Doing 100 runs for every temperature (except 0)
+        for question in QL:
+            # General query
+            query = "Below is a statement about you. Please read it and decide how much you agree or disagree with that statement. Write your response using the following scale:/n/n5 = strongly agree/n4 = agree/n3 = neutral/n2 = disagree/n1 = strongly disagree./n/nPlease answer the statement, even if you are not completely sure of your response./n/nStatement:"
+            response_prompt = "\nResponse:"
+            # Complete query
+            fullquestion = query + question + response_prompt
+            #print(fullquestion)
+            # Ask AI for output
+            response = openai.Completion.create(
+                model="text-davinci-002",
+                prompt=fullquestion,
+                temperature=temp,
+                max_tokens=max_tokens_meta,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                n=n_runs)
+
+            # Store answers into a list
+            answer_list = []
+            for n in range(n_runs):
+                # Convert response into string
+                g = str(response['choices'][n].text)
+                # Append string to list for this question
+                answer_list.append(g)
+
+            # Append current run answers to current temperature dataframe
+            tempcurrent[question] = answer_list
+        # Changing columns name for clarity
+        tempcurrent.columns = col_list
+        # Save temperature dataframe as csv
+        filename = "hexaco_non_reinforced_temperature_" + str(temp) + '.csv'
+        tempcurrent.to_csv(filename, index=False)
